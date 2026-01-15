15a2e6ea547469a2b9aec9edb7ead3091f77d415
Veeraj Matnale <veerajmatnale@gmail.com>
2026-01-15 12:01:16 +0530

    init

--- backend/app/db/mongo.py
+++ backend/app/db/mongo.py
@@ -4,15 +4,15 @@ from app.core.config import settings
 class MongoClient:
     client: AsyncIOMotorClient = None
 
-    mongo = MongoClient()
-    
-    async def connect_to_mongo(self):
-        mongo.client = AsyncIOMotorClient(settings.MONGO_URL)
-        print("Connected to MongoDB")
+mongo = MongoClient()
 
-        async def close_mongo_connection():
-            mongo.client.close()
-            print("Closed connection to MongoDB")
+async def connect_to_mongo():
+    mongo.client = AsyncIOMotorClient(settings.MONGO_URL)
+    print("Connected to MongoDB")
 
-            def get_db():
-                return mongo.client[settings.MONGO_DB_NAME]
+async def close_mongo_connection():
+    mongo.client.close()
+    print("Closed connection to MongoDB")
+
+def get_db():
+    return mongo.client[settings.MONGO_DB_NAME]
